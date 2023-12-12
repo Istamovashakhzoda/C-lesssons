cb3a4f8929843cbfb74dea08c74c83c9044e6795
Istamovashakhzoda <istamovashahzoda@gmail.com>
2023-12-12 20:49:00 +0500

    git commit

--- index.c
+++ index.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
 #include <string.h>
-
+// int main(int*nums, int*numsSize, int target, int*returnSize){
+//     i= 0;
+//     j=i+1;
+//     size1 = returnSize;
+//     size2=numsSize;
+//     while (i <size2 -1)
+//     {
+//     }
+    
+//     }
 // int main()
 // {
 //     int a = 9;
 //     int b = 5;
 //     int c = 3;
-//     if (a < b &&b = c)
+//     if (a > b &&b < c)
 //     {
-//         printf("small");
+//         printf("small\n");
 //     }
-//     else if (a < c && b != c)
+//     else if (a > c && b != c)
 //     {
-//         printf("nor big nor small");
+//         printf("nor big nor small\n");
 //     }
 //     else
 //     {
-//         printf("this number is big");
+//         printf("this number is big\n");
 //     }
 //     return 0;
 // }
@@ -43,9 +52,28 @@
 
 //     return 0;
 // }
-int i = 0;
-while (str[i] ! = '\0')
+// int i = 0;
+// while (str[i] ! = '\0')
+// {
+//     printf("%c\n", str[i]);
+//     i++;
+// }
+
+int maxProduct()
 {
-    printf("%c\n", str[i]);
-    i++;
+    int maxProduct(int*nums, int numsSize){
+    int ans=0;
+    for (int i = 0; i < numsSize(); i++)
+    {
+       for (int j=i+1; j < numsSize(); j++)
+       {
+       ans max(ans, (nums[i]-1)*(nums[j]-1));
+       }
+       
+    }
+
+ return ans;   
 }
+
+}
+    // ans max((nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12)
