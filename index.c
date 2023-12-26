975a7727d375f225268a7bcf089bddfc63689f61
Istamovashakhzoda <istamovashahzoda@gmail.com>
2023-12-26 20:39:37 +0500

    git next commit

--- index.c
+++ index.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 
+// #include <stdio.h>
 // int removeDuplicates(int arr[], int n) {
 //     if (n == 0) {
 //         return 0; 
@@ -34,3 +34,26 @@
 
 // The second exercise
 
+#include <stdio.h>
+#include <string.h>
+
+int lengthOfLastWord( char *str) {
+    int length = 0;
+    int i = strlen(str) - 1;
+    while (i >= 0 && str[i] != ' ') {
+        length++;
+        // i--;
+    }
+
+    return length;
+}
+int main() {
+     char *str = "Hello Worldes";
+    int length = lengthOfLastWord(str);
+    printf("Length of the last word: %d", length);
+
+    return 0;
+}
+    // while (i >= 0 && str[i] == ' ') {
+    //     i--;
+    // }
