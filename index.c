d3f55857ed5077489a2dfdd135408f6c9e993aec
Istamovashakhzoda <istamovashahzoda@gmail.com>
2023-12-27 10:43:15 +0500

    gi commit

--- index.c
+++ index.c
@@ -1,4 +1,3 @@
-
 // #include <stdio.h>
 // int removeDuplicates(int arr[], int n) {
 //     if (n == 0) {
@@ -40,20 +39,54 @@
 int lengthOfLastWord( char *str) {
     int length = 0;
     int i = strlen(str) - 1;
+    while (i >= 0 && str[i] == ' ') {
+        i--;
+    }
     while (i >= 0 && str[i] != ' ') {
         length++;
-        // i--;
+        i--;
     }
 
     return length;
 }
 int main() {
-     char *str = "Hello Worldes";
+     char *str = "Hello Shakhzoda";
     int length = lengthOfLastWord(str);
     printf("Length of the last word: %d", length);
 
     return 0;
 }
-    // while (i >= 0 && str[i] == ' ') {
-    //     i--;
-    // }
+
+
+// Third exersice from leetcode
+
+                                                                // NEW TOPIC  STRUCTURES
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include<string.h>
+
+//  typedef struct a_book
+// {
+// char*name;
+// int year;
+// char*author;
+// char*mainperson;
+// } book;
+
+// int main(){
+//    book*a_book = (book*)malloc(sizeof(book));
+//     a_book->author=malloc(sizeof(char)*30);
+//     a_book->name=malloc(sizeof(char)*23);
+//     a_book->year=2020;
+//     a_book->mainperson=malloc(sizeof(char)*20);
+//     strcpy(a_book->author,"Fyodor Dostoyeviskiy");
+//     strcpy(a_book->name,"Jinoyat va Jazo");
+//     strcpy(a_book->mainperson,"Rodion Raskolnikovning");
+//     printf("%s\n", a_book->author);
+//     printf("%s\n", a_book->name);
+//     printf("%d\n", a_book->year);
+//     printf("%s\n",a_book->mainperson);
+
+//     return 0;
+// }
