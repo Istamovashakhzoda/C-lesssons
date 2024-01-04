acb58b4c236cbe1e6f3145e61445702b5d3cf2fc
Istamovashakhzoda <istamovashahzoda@gmail.com>
2024-01-04 11:57:46 +0500

    git commit

--- index.c
+++ index.c
@@ -1,28 +1,28 @@
-// #include <stdio.h>
-// int removeDuplicates(int arr[], int n) {
+#include <stdio.h>
+// int select(int arr[], int n) {
 //     if (n == 0) {
 //         return 0; 
 //     }
 
-//     int uniqueIndex = 1; 
+//     int integer = 1; 
 
 //     for (int i = 1; i < n; i++) {
 //         if (arr[i] != arr[i-1]) {
-//             arr[uniqueIndex] = arr[i];
-//             uniqueIndex++;
+//             arr[integer] = arr[i];
+//             integer++;
 //         }
 //     }
 
-//     return uniqueIndex;
+//     return integer;
 // }
 
 // int main() {
 //     int arr[] = {1,1, 2, 2, 3, 4, 4, 4, 5, 6, 6};
 //     int n = sizeof(arr) / sizeof(arr[0]);
 
-//     int uniqueLength = removeDuplicates(arr, n);
+//      int lenght =select()
 
-//     for (int i = 0; i < uniqueLength; i++) {
+//     for (int i = 0; i < lenght; i++) {
 //         printf("%d ", arr[i]);
 //     }
 //     printf("\n");
@@ -32,30 +32,115 @@
 
 
 // The second exercise
+    // int lenght = select(arr, n);
 
-#include <stdio.h>
-#include <string.h>
+// #include <stdio.h>
+// #include <string.h>
 
-int lengthOfLastWord( char *str) {
-    int length = 0;
-    int i = strlen(str) - 1;
-    while (i >= 0 && str[i] == ' ') {
-        i--;
-    }
-    while (i >= 0 && str[i] != ' ') {
-        length++;
-        i--;
-    }
+// int uncommon( char *str) {
+//     int length = 0;
+//     int i = strlen(str) - 1;
+//     while (i >= 0 && str[i]!= ' ') {
+//         length++;
+//         i--;
+//     }
 
-    return length;
-}
-int main() {
-     char *str = "Hello Shakhzoda";
-    int length = lengthOfLastWord(str);
-    printf("Length of the last word: %d", length);
+//     return length;
+// }
+// int main() {
+//      char *str = "Hello Shakhzoda";
+//     int length = uncommon(str);
+//     printf("%d", length);
 
-    return 0;
-}
+//     return 0;
+// }
+
+//    MOVE ZEROES TASK FROM LEETCODE😭
+
+
+// void data(int arr[], int lenght);
+
+// int main(){
+//     int num[] = {0,9,0,8,5,0,3,0,1,0};
+//     int lenght = 11;
+//     for (int i = 0; i < lenght; i++)
+//     {
+//       printf("%d\n", num[i]);
+//       return 0;  
+//     }
+    
+//     void shift_zeros(int arr, int lenght){
+//         int coutn =0;
+//         for (int i = 0; i < lenght; i++);
+//         {
+//             if (arr[i] != 0);
+//        {
+//            arr[coutn] = arr[i];
+//            coutn++;
+//         }
+//         }
+//         for (int i = coutn; i < lenght; i++)
+//         {
+//               arr[i] = 0;
+//         }
+        
+        
+//     }
+    
+//     return 0;
+// }
+
+
+
+// #include <stdio.h>
+
+// void shift_zeros(int arr[], int length) {
+//     int count = 0;
+//     for (int i = 0; i < length; i++) {
+//         if (arr[i] != 0) {
+//             arr[count] = arr[i];
+//             count++;
+//         }
+//     }
+    
+//     for (int i = count; i < length; i++) {
+//         arr[i] = 0;
+//     }
+// }
+
+// int main() {
+//     int num[] = {0, 9, 0, 8, 5, 0, 3, 0, 1, 0};
+//     int length = 10;  
+
+//     printf("Original Array: ");
+//     for (int i = 0; i < length; i++) {
+//         printf("%d ", num[i]);
+//     }
+//     printf("\n");
+
+//     shift_zeros(num, length);
+
+//     printf("Array after shifting zeros: ");
+//     for (int i = 0; i < length; i++) {
+//         printf("%d ", num[i]);
+//     }
+//     printf("\n");
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Third exersice from leetcode
@@ -88,5 +173,154 @@ int main() {
 //     printf("%d\n", a_book->year);
 //     printf("%s\n",a_book->mainperson);
 
+//     return 0;
+// }
+
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int* make_array()
+// {
+//     int* arr = malloc(sizeof(int) * 10);
+//     int ind = 0;
+//     for (int i = -10; i < 0; i++)
+//     {
+//         arr[ind] = i;
+//         ind++;
+//     }
+//     return arr;
+// }
+
+// int main()
+// {
+//     int* arr = make_array();
+
+//     for (int i = 0; i < 10; i++)
+//     {
+//         printf("%d ", arr[i]);
+//     }
+//     printf("\n");
+//     free(arr);
+// }
+
+// ONE PLUS TASK FROM LEETCODE
+
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int *digit(int* digits, int digitSize, int*returnSize){
+//     int*result=(int*)malloc((digitSize+1)*sizeof(int));
+//     int plus = 1;
+//     for (int i = 0; i < digitSize-1; i>=0 i--){
+//         int sum = digits[i]+plus;
+//         result[i+1] = sum 10;
+//         plus= sum/10;
+//     }
+//     if (plus >0)
+//     {  
+//      result[0] = carry;
+//      returnSize=digitSize+1;     
+//     }
+//     else
+//     {
+//         for(int i=0; i < digitSize; i++){
+//             result[i]=digitSize[i]
+//         }
+//         returnSize=digitSize;
+//     }
+    
+//     return result;
+    
+// }
+
+// int main(){
+//     int digits[]={1,2,3,4};
+//     int digitSize = sizeof(digits)/sizeof(digits[0]);
+//     int returnSize;
+//     int* result = plusOne(digits, digitSize, returnSize);
+//     printf("");
+//     for (int i = 0; i < returnSize; i++)
+//     {
+//     printf("%d", result[i])    
+//     }
+//     printf("\n");
+//     return 0;
+// }
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// int *digit(int* digits, int digitSize, int* returnSize){
+// 	int*result=(int*)malloc((digitSize+1)*sizeof(int));
+// 	int plus = 1;
+// 	for (int i = 0; i < digitSize-1;  i--){
+// 	int sum = digits[i]+plus;
+// 	result[i+1] = sum %10;
+// 	plus= sum/10;
+// 	     }
+// 	     if (plus >0)
+// 	     {  
+// 	      result[0] = plus;
+// 	      *returnSize = digitSize+1;     
+// 	     }
+// 	     else
+// 	     {
+// 	         for(int i=0; i < digitSize; i++){
+// 	          result[i]=digits[i];
+// 	         }
+// 	         *returnSize=digitSize;
+// 	     }
+	    
+// 	     return result;
+	    
+// 	 }
+	
+// 	 int main(){
+// 	     int digits[]={1,2,3,4};	   
+//          int digitSize = sizeof(digits)/ sizeof(digits[0]);
+//         int returnSize;
+// 	     int* result = digit(digits, digitSize, &returnSize);
+// 	     printf("Result: ");
+// 	     for (int i = 0; i < returnSize; i++)
+// 	     {
+// 	     printf("%d", result[i]);  
+// 	     }
+// 	     printf("\n");
+//      return 0;
+// 	 }
+
+
+// int* digit(int* digits, int digitSize, int* returnSize) {
+//     int* result = (int*)malloc((digitSize + 1) * sizeof(int));
+//     int plus = 1;
+//     for (int i = digitSize - 1; i >= 0; i--) {
+//         int sum = digits[i] + plus;
+//         result[i + 1] = sum % 10;
+//         plus = sum / 10;
+//     }
+//     if (plus > 0) {
+//         result[0] = plus;
+//         *returnSize = digitSize + 1;
+//     } else {
+//         for (int i = 0; i < digitSize; i++) {
+//             result[i] = digits[i];
+//         }
+//         *returnSize = digitSize;
+//     }
+//     return result;
+// }
+
+// int main() {
+//     int digits[] = {1, 2, 3, 4};
+//     int digitSize = sizeof(digits) / sizeof(digits[0]);
+//     int returnSize;
+//     int* result = digit(digits, digitSize, &returnSize);
+//     printf("Result: ");
+//     for (int i = 0; i < returnSize; i++) {
+//         printf("%d ", result[i]);
+//     }
+//     printf("\n");
+//     free(result);
 //     return 0;
 // }
