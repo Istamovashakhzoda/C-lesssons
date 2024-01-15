79773252834a555ea0ebc4da53989bf63a168f4d
Istamovashakhzoda <istamovashahzoda@gmail.com>
2024-01-15 21:02:10 +0500

    git defined the problem from this code

--- index.c
+++ index.c
@@ -70,55 +70,46 @@
 // }
 
 // My code when i try to write this...
-#include<stdio.h>
-#include<fcntl.h>
-#include<unistd.h>
-#include<string.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h> // Added for strlen()
 
-void bubleSort(int arr[], int arrIndex){
-    int temp=0;
-    for (i = 0; i < arrIndex-1; i++)
-    {
-        for (j = i+1; i < arrIndex; j++)
-        {
-            if (arr[i]>arr[j])
-            {
-                temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
+void bubbleSort(int arr[], int arrIndex) {
+    int temp = 0;
+    for (int i = 0; i < arrIndex - 1; i++) {
+        for (int j = i + 1; j < arrIndex; j++) { // Fixed the typo here
+            if (arr[i] > arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
-            
         }
-        
     }
-    
 }
 
-void FullNull(char *str, int count){
-    for (int i = 0; i < count; i++)
-    {
-        str[i]='\n';
+void fillNull(char *str, int count) { // Renamed the function to follow lowercase naming convention
+    for (int i = 0; i < count; i++) {
+        str[i] = '\0'; // Fixed the assignment to null character
     }
-    
 }
 
-int *readNumbers(int*count){
-    int fileDescriptor = open("main", O_RDWR,);
+int *readNumbers(int *count) {
+    int fileDescriptor = open("main", O_RDWR); // Removed the extra comma
     int bufferIndex = 0;
     int arrIndex = 0;
     int bufferSize = 128;
     char character;
-    char *buffer = malloc(sizeof(char)*(bufferSize +1));
-    int *arr = malloc(sizeof(int)*(bufferSize+1));
+    char *buffer = malloc(sizeof(char) * (bufferSize + 1));
+    int *arr = malloc(sizeof(int) * bufferSize); // Changed the size to bufferSize
 
-    while (read(fileDescriptor, &character, 1)){
-        if (character=="," || character== '\n')
-        {
-           arr[arrIndex++] = atoi(buffer);
-           bufferIndex = 0;
-           FullNull(buffer, bufferSize);
-           continue;
+    while (read(fileDescriptor, &character, 1)) {
+        if (character == ',' || character == '\n') { // Changed the comparison operator
+            arr[arrIndex++] = atoi(buffer);
+            bufferIndex = 0;
+            fillNull(buffer, bufferSize);
+            continue;
         }
         buffer[bufferIndex++] = character;
     }
@@ -129,34 +120,31 @@ int *readNumbers(int*count){
     return arr;
 }
 
-void sortNums(int arr[], int arrIndex){
-    bubleSort(arr, arrIndex);
+void sortNums(int arr[], int arrIndex) {
+    bubbleSort(arr, arrIndex);
 
     int fileDescriptor = open("main", O_WRONLY | O_TRUNC);
-    char *buffer = malloc(sizeof(char) *128);
+    char *buffer = malloc(sizeof(char) * 128);
 
-    for (int bufferIndex = 0; bufferIndex < arrIndex; bufferIndex++)
-    {  
-    FullNull(buffer, 128);
-    sprintf(buffer, "%d", arr[bufferIndex]);
-    write(fileDescriptor, buffer, strlen(buffer));
-    if (bufferIndex < arrIndex -1)
-    {
-        write(fileDescriptor,",");
-    }
-    
+    for (int bufferIndex = 0; bufferIndex < arrIndex; bufferIndex++) {
+        fillNull(buffer, 128);
+        sprintf(buffer, "%d", arr[bufferIndex]);
+        write(fileDescriptor, buffer, strlen(buffer));
+        if (bufferIndex < arrIndex - 1) {
+            write(fileDescriptor, ",", 1); // Added the correct length for the comma
         }
-        write(fileDescriptor,"\n");
+    }
+    write(fileDescriptor, "\n", 1); // Added the correct length for the new line
 
-        free(buffer);
-        free(arr);
-        close(fileDescriptor);    
+    free(buffer);
+    free(arr);
+    close(fileDescriptor);
 }
 
-int main(){
+int main() {
     int count = 0;
-    int *numbers=readNumbers(&count);
+    int *numbers = readNumbers(&count);
     sortNums(numbers, count);
 
-    return 0 ;
+    return 0;
 }
