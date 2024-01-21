988cf07458c850dbf6d0201e197f795eff14f91a
Istamovashakhzoda <istamovashahzoda@gmail.com>
2024-01-21 19:30:50 +0500

    achieved

--- index.c
+++ index.c
@@ -1,180 +1,81 @@
-// #include<stdio.h>
-// #include<stdlib.h>
-// #include<string.h>
-// #include<fcntl.h>
-// #include<unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-// void bubbleSort(int array[], int arrayIndex){
-//     int temp = 0;
-//     for (int i = 0; i < arrayIndex-1; i++)
-//     {
-//         for (int j =  i +1; j < arrayIndex; j++)
-//         {
-//             if (array[i] = array[j])
-//             {
-//                 array[i] = temp;
-//                 array[i] = array[j];
-//                 array[j] = temp;
-//             }
-            
-//         }
-        
-//     }
-     
-// }
-
-// void FullNull(char *str, int count){
-//     for (int i = 0; i < count; i++)
-//     {
-//      str[i] = '\0';
-//     }
-// }
-
-
-// int *readNumbers(int *count){
-//     int fileDescreptor = open("main", O_RDWR);
-//     int bufferIndex = 0;
-//     int arrayIndex = 0;
-//     int bufferSize = 128;
-//     char character;
-//     char *buffer = malloc(sizeof(char) *(bufferSize+1));
-//     int *array = malloc(sizeof(int)*bufferSize);
-
-//     while (read(fileDescreptor, &character, 1))
-//     {
-//         if (character == ',' || character == '\n');
-//         array[arrayIndex] = atoi(buffer);
-//         bufferIndex = 0;
-//         FullNull(buffer, bufferSize);
-//         continue;
-//     }
-//     buffer[bufferIndex] = character;
-
-//     close(fileDescreptor);
-//     free(buffer);
-
-//     *count = arrayIndex;
-//     return array;
-// }
-
-
-// void SortNumbers(int array[], int arrayIndex){
-//     bubbleSort(array, arrayIndex);
-
-//     int fileDescreptor = open("main", O_WRONLY || O_TRUNC);
-//     char *buffer = malloc(sizeof(char) * 128);
-//     FullNull(buffer, 128);
-//     for (int bufferIndex = 0; bufferIndex < arrayIndex; bufferIndex++)
-//     {
-//     sprintf(buffer, "%d", array[bufferIndex]);
-//     write(fileDescreptor, buffer, strlen(buffer));
-//     if (bufferIndex<arrayIndex-1)
-//     {
-//         write(fileDescreptor, ",", 1);
-//     }
-//     FullNull(buffer,128);
-//     }
-//     write(fileDescreptor, "\n" , 1);
-//     free(buffer);
-//     free(array);
-//     close(fileDescreptor);
-// }
-
-// int main(){
-//     int count = 0;
-//     int *numbers = readNumbers(&count);
-//     SortNumbers(numbers, count);
-//     return 0;
-// }
-
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<fcntl.h>
-#include<unistd.h>
-
-void bubbleSort(int array[], int arrayIndex){
+void bubbleSort(int array[], int arrayIndex) {
     int temp = 0;
-    for (int i = 0; i < arrayIndex-1; i++)
-    {
-        for (int j = i+1; j < arrayIndex; j++)
-        {
-            if (array[i]> array[j])
-            {
-                array[i] = temp;
+    for (int i = 0; i < arrayIndex - 1; i++) {
+        for (int j = i + 1; j < arrayIndex; j++) {
+            if (array[i] > array[j]) {
+                temp = array[i];
                 array[i] = array[j];
                 array[j] = temp;
             }
-            
         }
-        
     }
-    
 }
 
-void FullNull(char *str, int count){
-    for (int i = 0; i < count; i++)
-    {
-        str[i]=0;
+void fillNull(char *str, int count) {
+    for (int i = 0; i < count; i++) {
+        str[i] = '\0';
     }
-    
 }
 
-int *readNum(int *count){
-    int fileDescreptor = open("main", O_RDWR);
+int *readNumbers(int *count) {
+    int fileDescriptor = open("main", O_RDONLY);
     int bufferIndex = 0;
     int arrayIndex = 0;
     int bufferSize = 128;
     char character;
-    char *buffer = malloc(sizeof(char) * (bufferSize+1));
+    char *buffer = malloc(sizeof(char) * (bufferSize + 1));
     int *array = malloc(sizeof(int) * bufferSize);
 
-    while (read(fileDescreptor, &character, 1))
-    {
-        if (character== ',' || character == '\n')
-        {
-            array[arrayIndex] = atoi(buffer);
+    while (read(fileDescriptor, &character, 1)) {
+        if (character == ',' || character == '\n') {
+            array[arrayIndex++] = atoi(buffer);
             bufferIndex = 0;
-            FullNull(buffer, bufferSize);
+            fillNull(buffer, bufferSize);
             continue;
         }
+        buffer[bufferIndex++] = character;
     }
-        buffer[bufferIndex ++] = character;
-        
-       close(fileDescreptor);
-       free(buffer);
 
-       *count = arrayIndex;
-       return 0;
+    close(fileDescriptor);
+    free(buffer);
+
+    *count = arrayIndex;
+    return array;
 }
 
-void sortNumbers(int array[], int arrayIndex){
+void sortNumbers(int array[], int arrayIndex) {
     bubbleSort(array, arrayIndex);
-    int fileDescreptor = open("main", O_WRONLY|| O_TRUNC);
+
+    int fileDescriptor = open("main", O_WRONLY | O_TRUNC);
     char *buffer = malloc(sizeof(char) * 128);
+    fillNull(buffer, 128);
 
-    for (int bufferIndex = 0; bufferIndex < arrayIndex; bufferIndex++)
-    {
-    FullNull(buffer, 128);
-        write(buffer, fileDescreptor, strlen(buffer));
+    for (int bufferIndex = 0; bufferIndex < arrayIndex; bufferIndex++) {
         sprintf(buffer, "%d", array[bufferIndex]);
-        if (bufferIndex <arrayIndex-1)
-        {
-        write(fileDescreptor, ',', 1);
+        write(fileDescriptor, buffer, strlen(buffer));
+        if (bufferIndex < arrayIndex - 1) {
+            write(fileDescriptor, ",", 1);
+            fillNull(buffer, 128);
         }
-        // FullNull(buffer, 128);
     }
-      write(fileDescreptor, '\n', 1);
-      close(fileDescreptor);
-      free(buffer);
-      free(array);
 
-    
+    write(fileDescriptor, "\n", 1);
+
+    free(buffer);
+    free(array);
+    close(fileDescriptor);
 }
 
-int main(){
+int main() {
     int count = 0;
-    int *numbers = readNum(&count);
+    int *numbers = readNumbers(&count);
     sortNumbers(numbers, count);
+
     return 0;
 }
